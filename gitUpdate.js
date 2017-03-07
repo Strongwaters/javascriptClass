c951dc8656ff0152764870a2d2535f0cd2f01ada
Mike Whitfield <mikewhitfield@Jasons-iMac-2.local>
2017-03-06 18:38:53 -0700

    initial

---
+++ gitUpdate.js
@@ -0,0 +1,17 @@
+/**
+ * This function should update a file of your choosing in your scheduler 
+ * folder.
+ */
+function updateFileInScheduler() {
+
+}
+
+/**
+ * Kicks off bash script to perform commit on new changes.
+ */
+function kickOffAddCommitPushScript() {
+
+}
+
+updateFileInScheduler();
+kickOffAddCommitPushScript();
